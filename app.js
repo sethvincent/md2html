e532154d13d43f34ff6423c4659ccb6d594bb1ee
sethvincent <sethvincent@gmail.com>
2013-06-15 19:17:27 -0700

    add option to use marked.js

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var markdown_area = document.getElementById('editor');
 var html_area = document.getElementById('generated-html');
-var button = document.getElementById('convert');
+var github_button = document.getElementById('github-convert');
+var marked_button = document.getElementById('marked-convert');
 
-button.addEventListener('click', convert);
+github_button.addEventListener('click', githubConverter);
+marked_button.addEventListener('click', markedConverter);
 
-function convert(){
+marked.setOptions({
+  gfm: false,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false,
+  langPrefix: 'language-',
+  highlight: function(code, lang) {
+    if (lang === 'js') {
+      return highlighter.javascript(code);
+    }
+    return code;
+  }
+});
+
+function markedConverter(){
+  var md = marked(markdown_area.value);
+  html_area.value = md;
+}
+
+function githubConverter(){
   reqwest({
     url: 'https://api.github.com/markdown/raw', 
     method: 'post',
