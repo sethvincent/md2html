ca94d0ac59ec9d7d82dc4a2c5a60e1e0fecaee89
sethvincent <sethvincent@gmail.com>
2013-06-15 19:24:24 -0700

    revise description

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 var markdown_area = document.getElementById('editor');
 var html_area = document.getElementById('generated-html');
+
 var github_button = document.getElementById('github-convert');
 var marked_button = document.getElementById('marked-convert');
 
 github_button.addEventListener('click', githubConverter);
 marked_button.addEventListener('click', markedConverter);
-
-marked.setOptions({
-  gfm: false,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: true,
-  smartLists: true,
-  smartypants: false,
-  langPrefix: 'language-',
-  highlight: function(code, lang) {
-    if (lang === 'js') {
-      return highlighter.javascript(code);
-    }
-    return code;
-  }
-});
 
 function markedConverter(){
   var md = marked(markdown_area.value);
